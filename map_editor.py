336595461ba719fb2dece60b13c2b7bec9441f0d
Sean Shin <seanshin2647@gmail.com>
2019-06-23 21:25:00 -0600

    Added an empty parent class for all editor states. The class State will be a parent class to all other class states.

--- map_editor.py
+++ map_editor.py
@@ -3,4 +3,16 @@ from map_assets import *
 def end_program():
     pygame.quit()
     quit()
-    
+
+class State():
+    def __init__(self):
+        pass
+
+    def render(self):
+        raise NotImplementedError
+
+    def update(self):
+        raise NotImplementedError
+
+    def handle_events(self):
+        raise NotImplementedError
