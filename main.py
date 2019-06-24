29c561301538eebc832c9e59f3927f930c7fe050
Sean Shin <seanshin2647@gmail.com>
2019-06-23 20:42:22 -0600

    Added code to create a display. I have set the dimensions to 600x600 for now. I plan to make that changeable through a JSON or text file that the user could edit to make the window a different size. The reason is that the display will be divided into grids with squares of 10x10 pixels. The user will probably also be allowed to change that as well through a JSON or text file.

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 from import_libraries import *
+
+pygame.init()
+
+#TODO: Make the user be able to change the display width and height. 
+# Possibly use a .txt file or JSON.
+FPS = 30
+DISPLAY_WIDTH = 600
+DISPLAY_HEIGHT = 600
+DISPLAY = pygame.display.set_mode([DISPLAY_WIDTH, DISPLAY_HEIGHT])
+
+clock = pygame.time.Clock()
