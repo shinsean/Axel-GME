8c6073b62000129bebf929caa688c494d4c1345c
Sean Shin <seanshin2647@gmail.com>
2019-06-23 23:26:29 -0600

    Added the __init__ for the class Block. The Block class will be a 10x10 square that will be placed using the given x and y coordinates that will be calculated by flooring the coordinate numbers of where the user will click.

--- map_assets.py
+++ map_assets.py
@@ -1,1 +1,14 @@
 from import_libraries import *
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self, x_position, y_position):
+        super().__init__()
+
+        self.side_length = 10
+
+        self.image = pygame.Surface([self.side_length, self.side_length])
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+
+        self.rect.x = x_position
+        self.rect.y = y_position
