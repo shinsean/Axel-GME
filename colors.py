f1e001b82cdaaad13d8a5c9ca176d1c0dbc03de4
Sean Shin <seanshin2647@gmail.com>
2019-06-23 20:32:46 -0600

    Added the colors in RGB to colors.py

---
+++ colors.py
@@ -0,0 +1,45 @@
+# Created for my own convenience.
+# By Sean Shin
+
+# Basic colors.
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# Shades of white and black.
+SILVER = (192, 192, 192)
+GRAY = (128, 128, 128)
+
+# Basic RGB colors
+PURE_RED = (255, 0, 0)
+PURE_GREEN = (0, 255, 0) #Lime
+PURE_BLUE = (0, 0, 255)
+
+# Toned down RGB colors.
+# Looks more normal.
+RED = (175, 0, 0)
+GREEN = (0, 175, 0)
+BLUE = (0, 0, 175)
+
+# Dark RGB colors.
+DARK_RED = (80, 0, 0)
+DARK_GREEN = (0, 80, 0)
+DARK_BLUE = (0, 0, 80)
+
+# Light RGB colors.
+LIGHT_RED = (255, 0, 0)
+LIGHT_GREEN = (0, 255, 0) #Lime
+LIGHT_BLUE = (0, 0, 255)
+
+# Shades of blue and green.
+TURQUOISE = (0, 192, 192)
+
+# Basic secondary colors.
+PURE_YELLOW = (255, 255, 0) 
+PURE_ORANGE = (255, 165, 0)
+PURE_PURPLE = (255, 0, 255) #Magenta
+
+# Toned down secondary colors.
+# Looks more normal.
+YELLOW = (210, 210, 0)
+ORANGE = (255, 165, 0)
+PURPLE = (128, 0, 128)
