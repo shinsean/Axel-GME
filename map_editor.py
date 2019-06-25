ace460903940c925213184f0efff80e2efc723e5
Sean Shin <seanshin2647@gmail.com>
2019-06-24 22:58:50 -0600

    Added pygame groups to use later. I have added all_sprites_list so that I can add every sprite to it and update them all in one method. I have added wall_list in case I need it later for calculations and logic regarding walls.

--- map_editor.py
+++ map_editor.py
@@ -21,3 +21,7 @@ class Game_State(State):
     def __init__(self):
         # Will add more here as I make the program.
         super().__init__()
+
+        self.all_sprites_list = pygame.sprite.Group()
+        # Not exactly sure yet what I need this for.
+        self.wall_list = pygame.sprite.Group()
