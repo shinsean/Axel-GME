bf1387521cf8e7aa63d848c9e29f2043a8da184e
Sean Shin <seanshin2647@gmail.com>
2019-07-01 13:45:30 -0600

    Added set_grid_positions to Block. When a block is created, the raw_x and raw_y from the user's mouse click will be passed in as an argument. The raw_x and raw_y will be used to set the rect.x and rect.y. The raw_x will be divided by the side_length and then changed to an integer. This will first change the raw_x and raw_y into a small number with a decimal(s). Secondly, it will lop off any decimal points. After that, the resulting number will be multiplied by the side_legnth to make its position align with other possible blocks.

--- map_assets.py
+++ map_assets.py
@@ -1,14 +1,22 @@
 from import_libraries import *
 
 class Block(pygame.sprite.Sprite):
-    def __init__(self, x_position, y_position):
+    def __init__(self, raw_x, raw_y):
         super().__init__()
 
         self.side_length = 10
 
+        self.raw_x = raw_x
+        self.raw_y = raw_y
+
         self.image = pygame.Surface([self.side_length, self.side_length])
         self.image.fill(GREEN)
         self.rect = self.image.get_rect()
+        self.set_grid_positions()
 
-        self.rect.x = x_position
-        self.rect.y = y_position
+    def set_grid_positions(self):
+        self.rect.x = (int(self.raw_x/self.side_length)
+            * self.side_length)
+        
+        self.rect.y = (int(self.raw_y/self.side_length)
+            * self.side_length)
