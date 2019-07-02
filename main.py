6531ec1b074f24c99d62a221370cf4d614b76b2e
Sean Shin <seanshin2647@gmail.com>
2019-07-01 22:35:40 -0600

    Added a main_loop to run the program.

--- main.py
+++ main.py
@@ -10,3 +10,20 @@ DISPLAY_HEIGHT = 600
 DISPLAY = pygame.display.set_mode([DISPLAY_WIDTH, DISPLAY_HEIGHT])
 
 clock = pygame.time.Clock()
+
+def main_loop():
+    program_state = Editing_State()
+    
+    while True:
+        pressed_buttons = pygame.key.get_pressed()
+        program_state.handle_events(pressed_buttons)
+
+        # Placeholder in case of the future.
+        program_state.update()
+
+        program_state.render(DISPLAY)
+
+        pygame.display.update()
+        clock.tick(FPS)
+
+main_loop()
