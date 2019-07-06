7903f9cae9c0b6edb367e617f92f966afa2fb495
Sean Shin <seanshin2647@gmail.com>
2019-07-05 23:39:31 -0600

    Added a function to save the current map. The function will take in a list as an argument. The list that is passed in will be the list that is used to keep track of the state of the map. When writing into the txt file, to add newlines, a function named insert_newlines will be called and passed in map_list_rep as an argument.

--- map_saver.py
+++ map_saver.py
@@ -2,8 +2,9 @@ from map_assets import *
 
 # TODO: Have a way to save whiel creating a new file.
 def save_current_map(map_list_rep):
+    map_list_rep_edit = insert_newlines(map_list_rep)
     open_save_file = open('current_save.txt', 'w+')
-    for rows in map_list_rep:
-        for letter in map_list_rep[0]:
+    for rows in map_list_rep_edit:
+        for letter in rows:
             open_save_file.write(letter)
     open_save_file.close()
