b3863f4a5ae1c2ddb31054fd2cd37203c0172ea0
Sean Shin <seanshin2647@gmail.com>
2019-09-07 15:41:53 -0600

    Create method for loading previously saved blocks

--- map_editor.py
+++ map_editor.py
@@ -61,6 +61,16 @@ class Editing_State(State):
 
         save_current_map(self.map_list)
 
+    def create_loaded_blocks(self, block_side_length):
+        for y_index in range(self.map_list):
+            for x_index in range(rows):
+                if self.map_list[y_index][x_index] == 'X':
+                    self.raw_x = columns * block_side_length
+                    self.raw_y = columns * block_side_length
+                    self.block = Block(self.raw_x, self.raw_y)
+                    self.wall_list.add(self.block)
+                    self.all_sprites_list.add(self.block)
+
     # Core function.
     def render(self, display):
         self.all_sprites_list.draw(display)
