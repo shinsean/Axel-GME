a2c1a4a3fb0933ff853fd86bf538ebf9fc01236c
Sean Shin <seanshin2647@gmail.com>
2019-09-11 20:33:07 -0600

    Rename "test_file.txt" to "save_file.txt"

--- map_saver.py
+++ map_saver.py
@@ -2,7 +2,7 @@ from map_assets import *
 
 # TODO: Have a way to save whiel creating a new file.
 def save_current_map(map_list_rep):
-    open_save_file = open('test_save.txt', 'w')
+    open_save_file = open('save_file.txt', 'w')
     for rows in map_list_rep:
         for letter in rows:
             open_save_file.write(letter)
